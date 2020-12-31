edaa82b95bd19319519021b51983d230971d4717
MarcRosellH <36154632+MarcRosellH@users.noreply.github.com>
2020-12-31 11:55:27 +0100

    Ak files imported and working again plus config change

--- Camel Engine/Source/WwiseLibrary.h
+++ Camel Engine/Source/WwiseLibrary.h
@@ -2,7 +2,7 @@
 #include "Globals.h"
 #include "AK/SoundEngine/Common/AkSoundEngine.h"
 #include "AK/IBytes.h"
-#include <AK/SoundEngine/Common/AkTypes.h>
+#include "AK/SoundEngine/Common/AkTypes.h"
 
 #ifndef _DEBUG
 
@@ -15,7 +15,7 @@
 #pragma comment( lib, "AK/Debug(StaticCRT)/lib/AkRoomVerbFX.lib")
 
 #else
-#include <AK/Comm/AkCommunication.h>
+#include "AK/Comm/AkCommunication.h"
 #pragma comment( lib, "AK/Debug(StaticCRT)/lib/CommunicationCentral.lib")
 #pragma comment( lib, "AK/ws2_32.lib") 
 #pragma comment( lib, "AK/Debug(StaticCRT)/lib/AkSoundEngine.lib")
@@ -31,16 +31,14 @@
 #pragma comment( lib, "AK/dsound.lib" ) 
 #pragma comment( lib, "AK/dxguid.lib" )
 
-//namespace AK
-//{
-//#ifdef WIN32
-//	void* AllocHook(size_t in_size);
-//	void FreeHook(void* in_ptr);
-//
-//	void* VirtualAllocHook(void* in_pMemAddress, size_t in_size, DWORD in_dwAllocationType, DWORD in_dwProtect);
-//	void VirtualFreeHook(void* in_pMemAddress, size_t in_size, DWORD in_dwFreeType);
-//}
-//#endif
+namespace AK
+{
+	void* AllocHook(size_t in_size);
+	void FreeHook(void* in_ptr);
+
+	void* VirtualAllocHook(void* in_pMemAddress, size_t in_size, DWORD in_dwAllocationType, DWORD in_dwProtect);
+	void VirtualFreeHook(void* in_pMemAddress, size_t in_size, DWORD in_dwFreeType);
+}
 
 bool InitSoundEngine();
 void ProcessAudio();
